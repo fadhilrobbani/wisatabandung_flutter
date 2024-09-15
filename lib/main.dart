3d07429dfe71b1156d0bf663aac1874d98118edf
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-09-15 17:48:03 +0700

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,60 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Wisata Bandung',
+        theme: ThemeData(),
+        home: const FormScreen(),
+      );
+}
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  String _name = '';
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wisata Bandung',
-      theme: ThemeData(),
-      home: const DetailScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _name = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text('Hello, $_name'),
+                      );
+                    });
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
@@ -70,6 +119,20 @@ class DetailScreen extends StatelessWidget {
               'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
               textAlign: TextAlign.justify,
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    WidgetStateProperty.all<Color>(Colors.blueAccent),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Submit',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           )
         ],
